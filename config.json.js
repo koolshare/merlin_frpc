d727bb29cca885bd0db76007a82d228b4d22d946
clang <clang@debian>
2017-02-03 10:49:15 +0800

    frpc 1.9.7

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "内网穿透利器，谁用谁知道。", 
     "home_url": "Module_frpc.asp", 
-    "md5": "b3e96e28d1de1dacea0175a29e151974", 
+    "md5": "2f160fc8217da18ba20663738f876c2d", 
     "module": "frpc", 
     "title": "frpc穿透", 
-    "version": "1.9.6"
+    "version": "1.9.7"
 }
--- frpc/install.sh
+++ frpc/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 MODULE=frpc
-VERSION="1.9.6"
+VERSION="1.9.7"
 cd /
 rm -f /koolshare/init.d/S98frpc.sh
 cp -f /tmp/$MODULE/bin/* /koolshare/bin/
