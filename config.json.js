be7dfdcb456d1665b9f66e5711b3415ba53d8bda
clangcn <clangcn@gmail.com>
2018-05-11 15:08:57 +0800

    merlin_frpc 2.1.11

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "内网穿透利器，谁用谁知道。", 
     "home_url": "Module_frpc.asp", 
-    "md5": "b4571e3c9bd87f925c63a8e573992e06", 
+    "md5": "ae45738e9ecb69ab9813625de8d2b954", 
     "module": "frpc", 
     "title": "frpc穿透", 
-    "version": "2.1.10"
+    "version": "2.1.11"
 }
--- frpc/install.sh
+++ frpc/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 MODULE=frpc
-VERSION="2.1.10"
+VERSION="2.1.11"
 cd /tmp
 rm -f /koolshare/init.d/S98frpc.sh
 if [ ! -x /koolshare/bin/base64_encode ]; then
